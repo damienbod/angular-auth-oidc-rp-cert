d2b53dc3fb566ab27ee5494b0581d197ec457b20
damienbod <damien_bod@hotmail.com>
2017-06-21 11:13:06 +0200

    init version for the testing

---
+++ src/AngularClient/angularApp/app/app.component.ts
@@ -0,0 +1,76 @@
+﻿import { Http, Response, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Configuration } from './app.constants';
+import { OidcSecurityService, AuthConfiguration } from 'angular-auth-oidc-client';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { HomeComponent } from './home/home.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+
+import './app.component.css';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent implements OnInit {
+
+    constructor(
+        public securityService: OidcSecurityService,
+        private http: Http,
+        private authConfiguration: AuthConfiguration) {
+    }
+
+    ngOnInit() {
+        if (window.location.hash) {
+            this.securityService.authorizedCallback();
+        }
+    }
+
+    login() {
+        this.securityService.authorize();
+        //// https://rp.certification.openid.net:8080/damienbod.id_token-token/rp-response_type-id_token+token/registration
+        //console.log('start login');
+        //this.registerClient()
+        //    .subscribe((data: any) => {
+        //        this.authConfiguration.client_id = data.client_id;
+        //        console.log(data);
+        //        this.securityService.authorize();
+        //    });
+    }
+
+    refreshSession() {
+        console.log('start refreshSession');
+        this.securityService.authorize();
+    }
+
+    logout() {
+        console.log('start logoff');
+        this.securityService.logoff();
+    }
+
+    //private registerClient = (): Observable<any> => {
+
+    //    let headers = new Headers();
+    //    headers.append('Content-Type', 'application/json');
+    //    headers.append('Accept', 'application/json');
+    //    headers.append('access-control-allow-origin', '*');
+
+    //    let url = 'https://rp.certification.openid.net:8080/damienbod.id_token-token/rp-response_type-id_token+token/registration'; // this.testurl + '/registration';
+    //    let jsonbody = '{ "application_type": "web", "redirect_uris": [ "https://localhost:44386" ], "response_types": [ "id_token", "id_token token" ]}';
+    //    console.log(url);
+    //    console.log(jsonbody);
+
+    //    return this.http.post(url, {
+    //        headers: headers,
+    //        body: jsonbody
+    //    }).map((res: any) => res.json());
+    //}
+}
---
+++ src/AngularClient/angularApp/app/app.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Configuration } from './app.constants';
+import { routing } from './app.routes';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { HomeComponent } from './home/home.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+
+import { AuthModule, OidcSecurityService, AuthConfiguration } from 'angular-auth-oidc-client';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        routing,
+        HttpModule,
+        JsonpModule,
+        AuthModule.forRoot(),
+    ],
+    declarations: [
+        AppComponent,
+        ForbiddenComponent,
+        HomeComponent,
+        UnauthorizedComponent
+    ],
+    providers: [
+        OidcSecurityService,
+        AuthConfiguration,
+        Configuration
+    ],
+    bootstrap:    [AppComponent],
+})
+
+export class AppModule {
+    constructor(public authConfiguration: AuthConfiguration) {
+        this.authConfiguration.stsServer = 'https://rp.certification.openid.net:8080/damienbod.id_token_token/rp-id_token-kid-absent-multiple-jwks';
+        this.authConfiguration.redirect_url = 'https://localhost:44386';
+        // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
+        // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
+        this.authConfiguration.client_id = 'GBkNhByHPmO2';
+        this.authConfiguration.response_type = 'id_token token';
+        this.authConfiguration.scope = 'openid email profile';
+        this.authConfiguration.post_logout_redirect_uri = 'https://localhost:44386/Unauthorized';
+        this.authConfiguration.start_checksession = false;
+        this.authConfiguration.silent_renew = true;
+        this.authConfiguration.startup_route = '/home';
+        // HTTP 403
+        this.authConfiguration.forbidden_route = '/Forbidden';
+        // HTTP 401
+        this.authConfiguration.unauthorized_route = '/Unauthorized';
+        this.authConfiguration.log_console_warning_active = true;
+        this.authConfiguration.log_console_debug_active = true;
+        // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
+        // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
+        this.authConfiguration.max_id_token_iat_offset_allowed_in_seconds = 5;
+        this.authConfiguration.override_well_known_configuration = false;
+        this.authConfiguration.override_well_known_configuration_url = 'https://localhost:44386/wellknownconfiguration.json';
+    }
+}
