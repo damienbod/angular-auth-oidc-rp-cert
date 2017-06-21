d9065c3c9fcc7052bfc6f9aca4893f45d035d15f
damienbod <damien_bod@hotmail.com>
2017-06-21 16:02:06 +0200

    more tests

--- src/AngularClient/angularApp/app/app.module.ts
+++ src/AngularClient/angularApp/app/app.module.ts
@@ -37,11 +37,11 @@ import { AuthModule, OidcSecurityService, AuthConfiguration } from 'angular-auth
 
 export class AppModule {
     constructor(public authConfiguration: AuthConfiguration) {
-        this.authConfiguration.stsServer = 'https://rp.certification.openid.net:8080/angular-auth-oidc-client.id_token/rp-id_token-bad-sig-rs256';
+        this.authConfiguration.stsServer = 'https://rp.certification.openid.net:8080/angular-auth-oidc-client.id_token/rp-id_token-issuer-mismatch';
         this.authConfiguration.redirect_url = 'https://localhost:44386';
         // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
         // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
-        this.authConfiguration.client_id = '5mYGifuLOZb9';
+        this.authConfiguration.client_id = 'QOQrdQGHudOr';
         this.authConfiguration.response_type = 'id_token';
         this.authConfiguration.scope = 'openid email profile';
         this.authConfiguration.post_logout_redirect_uri = 'https://localhost:44386/Unauthorized';
