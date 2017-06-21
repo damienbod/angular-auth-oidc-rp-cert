a35fbc8662480afd13ef4e455128f92320089a16
damienbod <damien_bod@hotmail.com>
2017-06-21 16:21:00 +0200

    id_token tests complete

--- src/AngularClient/angularApp/main.js
+++
@@ -1,9 +0,0 @@
-import './vendor';
-import './polyfills';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-if (module.hot) {
-    module.hot.accept();
-}
-platformBrowserDynamic().bootstrapModule(AppModule);
-//# sourceMappingURL=main.js.map
