a7d653c4f3e63d4a88e958dcbb4499e57c93c7d8
damienbod <damien_bod@hotmail.com>
2017-08-09 15:52:20 +0200

    updating auth module to 1.3.2

--- src/AngularClient/angularApp/app/app.component.ts
+++ src/AngularClient/angularApp/app/app.component.ts
@@ -5,13 +5,14 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Configuration } from './app.constants';
 import { OidcSecurityService, AuthConfiguration } from 'angular-auth-oidc-client';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { HomeComponent } from './home/home.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { Subscription } from 'rxjs/Subscription';
 
 import './app.component.css';
 
@@ -20,7 +21,10 @@ import './app.component.css';
     templateUrl: 'app.component.html'
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+
+    isAuthorizedSubscription: Subscription;
+    isAuthorized: boolean;
 
     constructor(
         public securityService: OidcSecurityService,
@@ -29,9 +33,18 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.isAuthorizedSubscription = this.securityService.getIsAuthorized().subscribe(
+            (isAuthorized: boolean) => {
+                this.isAuthorized = isAuthorized;
+            });
+
         if (window.location.hash) {
             this.securityService.authorizedCallback();
         }
+    }
+
+    ngOnDestroy(): void {
+        this.isAuthorizedSubscription.unsubscribe();
     }
 
     login() {
