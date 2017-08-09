3daa401cea447e3ee06508d2757e5b8319801b2c
damienbod <damien_bod@hotmail.com>
2017-08-09 14:47:46 +0200

    updating test app

--- src/AngularClient/angularApp/app/app.module.ts
+++ src/AngularClient/angularApp/app/app.module.ts
@@ -10,7 +10,7 @@ import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { HomeComponent } from './home/home.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
-import { AuthModule, OidcSecurityService, AuthConfiguration } from 'angular-auth-oidc-client';
+import { AuthModule, OidcSecurityService, OpenIDImplicitFlowConfiguration } from 'angular-auth-oidc-client';
 
 @NgModule({
     imports: [
@@ -29,35 +29,39 @@ import { AuthModule, OidcSecurityService, AuthConfiguration } from 'angular-auth
     ],
     providers: [
         OidcSecurityService,
-        AuthConfiguration,
         Configuration
     ],
     bootstrap:    [AppComponent],
 })
 
 export class AppModule {
-    constructor(public authConfiguration: AuthConfiguration) {
-        this.authConfiguration.stsServer = 'https://rp.certification.openid.net:8080/angular-auth-oidc-client.config/rp-discovery-openid-configuration';
-        this.authConfiguration.redirect_url = 'https://localhost:44386';
+    constructor(public oidcSecurityService: OidcSecurityService) {
+
+        let openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
+        openIDImplicitFlowConfiguration.stsServer = 'https://rp.certification.openid.net:8080/angular-auth-oidc-client.config/rp-discovery-issuer-not-matching-config';
+
+        openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44386';
         // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
         // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
-        this.authConfiguration.client_id = 'v4UmV1nGHaVc';
-        this.authConfiguration.response_type = 'id_token token';
-        this.authConfiguration.scope = 'openid email profile';
-        this.authConfiguration.post_logout_redirect_uri = 'https://localhost:44386/Unauthorized';
-        this.authConfiguration.start_checksession = false;
-        this.authConfiguration.silent_renew = true;
-        this.authConfiguration.startup_route = '/home';
+        openIDImplicitFlowConfiguration.client_id = 'nOFZgmMJuYiS';
+        openIDImplicitFlowConfiguration.response_type = 'id_token token';
+        openIDImplicitFlowConfiguration.scope = 'openid';
+        openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'https://localhost:44386/Unauthorized';
+        openIDImplicitFlowConfiguration.start_checksession = false;
+        openIDImplicitFlowConfiguration.silent_renew = true;
+        openIDImplicitFlowConfiguration.startup_route = '/home';
         // HTTP 403
-        this.authConfiguration.forbidden_route = '/Forbidden';
+        openIDImplicitFlowConfiguration.forbidden_route = '/Forbidden';
         // HTTP 401
-        this.authConfiguration.unauthorized_route = '/Unauthorized';
-        this.authConfiguration.log_console_warning_active = true;
-        this.authConfiguration.log_console_debug_active = true;
+        openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
+        openIDImplicitFlowConfiguration.log_console_warning_active = true;
+        openIDImplicitFlowConfiguration.log_console_debug_active = true;
         // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
         // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
-        this.authConfiguration.max_id_token_iat_offset_allowed_in_seconds = 5;
-        this.authConfiguration.override_well_known_configuration = false;
-        this.authConfiguration.override_well_known_configuration_url = 'https://localhost:44386/wellknownconfiguration.json';
+        openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 5;
+        openIDImplicitFlowConfiguration.override_well_known_configuration = false;
+        openIDImplicitFlowConfiguration.override_well_known_configuration_url = 'https://localhost:44386/wellknownconfiguration.json';
+
+        this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration);
     }
 }
