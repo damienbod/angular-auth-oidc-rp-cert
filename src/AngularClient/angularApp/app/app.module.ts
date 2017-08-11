f4263239bf8e349577f006618a0ef29139c1cecf
damienbod <damien_bod@hotmail.com>
2017-08-11 14:11:58 +0200

    rp-discovery-issuer-not-matching-config

--- src/AngularClient/angularApp/app/app.module.ts
+++ src/AngularClient/angularApp/app/app.module.ts
@@ -38,12 +38,12 @@ export class AppModule {
     constructor(public oidcSecurityService: OidcSecurityService) {
 
         let openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
-        openIDImplicitFlowConfiguration.stsServer = 'https://rp.certification.openid.net:8080/angular-auth-oidc-client.config/rp-discovery-openid-configuration';
+        openIDImplicitFlowConfiguration.stsServer = 'https://rp.certification.openid.net:8080/angular-auth-oidc-client.config/rp-discovery-issuer-not-matching-config';
 
         openIDImplicitFlowConfiguration.redirect_url = 'https://localhost:44386';
         // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
         // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
-        openIDImplicitFlowConfiguration.client_id = 'dgq25Vf3C9lg';
+        openIDImplicitFlowConfiguration.client_id = 'FTMuhVTZ61bG';
         openIDImplicitFlowConfiguration.response_type = 'id_token token';
         openIDImplicitFlowConfiguration.scope = 'openid profile';
         openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'https://localhost:44386/Unauthorized';
